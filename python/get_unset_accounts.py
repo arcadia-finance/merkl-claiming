3f61737d49d5aa007dc179d615f39ed14b6ac7b8
j-vp <jasper@arcadia.finance>
2024-08-19 09:28:15 +0200

    🎨 cleanup

--- python/get_unset_accounts.py
+++ python/get_unset_accounts.py
@@ -5,47 +5,65 @@ import dotenv
 from multicall import Multicall, Call
 import asyncio
 
-dotenv.load_dotenv(dotenv_path='.env')
-RPC_URL = os.getenv('RPC_URL')
+dotenv.load_dotenv(dotenv_path=".env")
+RPC_URL = os.getenv("RPC_URL")
 
 w3 = Web3(Web3.HTTPProvider(RPC_URL))
 
-with open('jsons/distributor_abi.json') as f:
+with open("jsons/distributor_abi.json") as f:
     distributor_abi = json.load(f)
 
-with open('jsons/arcadia_factory_abi.json') as f:
+with open("jsons/arcadia_factory_abi.json") as f:
     arcadia_factory_abi = json.load(f)
 
-distributor = w3.eth.contract(address='0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae', abi=distributor_abi)
-arcadia_factory = w3.eth.contract(address='0xDa14Fdd72345c4d2511357214c5B89A919768e59', abi=arcadia_factory_abi)
+distributor = w3.eth.contract(
+    address="0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae", abi=distributor_abi
+)
+arcadia_factory = w3.eth.contract(
+    address="0xDa14Fdd72345c4d2511357214c5B89A919768e59", abi=arcadia_factory_abi
+)
 CLAIMER = "0x3146e7bCeE81aE5a9BDcC8452ba7bBf9f8524205"
+
 
 async def get_accounts(amount_of_accounts):
     def from_value(value):
-        return Web3.to_checksum_address(value) 
-    
+        return Web3.to_checksum_address(value)
+
     accounts = []
     index_arr = list(range(amount_of_accounts))
     for i in range(0, amount_of_accounts, 100):
-        batch = index_arr[i:i+100]
+        batch = index_arr[i : i + 100]
         calls = [
-            Call("0xDa14Fdd72345c4d2511357214c5B89A919768e59", ["allAccounts(uint256)(address)", index], [(str(index), from_value)]) for index in batch
+            Call(
+                "0xDa14Fdd72345c4d2511357214c5B89A919768e59",
+                ["allAccounts(uint256)(address)", index],
+                [(str(index), from_value)],
+            )
+            for index in batch
         ]
         multicall = Multicall(calls, _w3=w3)
         results = await multicall.coroutine()
         accounts.extend([results[str(x)] for x in batch])
     return accounts
 
+
 async def get_operator_status(accounts):
     is_operator_set = {}
     for i in range(0, len(accounts), 100):
-        batch = accounts[i:i+100]
+        batch = accounts[i : i + 100]
         calls = [
-            Call("0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae", ["operators(address,address)(bool)", account, CLAIMER], [(account, None)]) for account in batch
+            Call(
+                "0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae",
+                ["operators(address,address)(bool)", account, CLAIMER],
+                [(account, None)],
+            )
+            for account in batch
         ]
         multicall = Multicall(calls, _w3=w3)
         results = await multicall.coroutine()
-        is_operator_set.update({account: True if results[account] == 1 else False for account in batch})
+        is_operator_set.update(
+            {account: True if results[account] == 1 else False for account in batch}
+        )
 
     return is_operator_set
 
@@ -55,12 +73,15 @@ async def get_accounts_without_operator():
     all_accounts = await get_accounts(amount_of_accounts)
 
     is_operator_set = await get_operator_status(all_accounts)
-    accounts_to_set_operator = [account for account, value in is_operator_set.items() if value is False]    
+    accounts_to_set_operator = [
+        account for account, value in is_operator_set.items() if value is False
+    ]
 
-    with open('accounts_unset_operators.json', '+w') as f:
+    with open("accounts_unset_operators.json", "+w") as f:
         f.write(json.dumps(accounts_to_set_operator))
 
     return accounts_to_set_operator
+
 
 if __name__ == "__main__":
     asyncio.run(get_accounts_without_operator())
--- python/main.py
+++ python/main.py
@@ -5,18 +5,21 @@ import os
 import dotenv
 import asyncio
 
-dotenv.load_dotenv(dotenv_path='.env')
-RPC_URL = os.getenv('RPC_URL')
+dotenv.load_dotenv(dotenv_path=".env")
+RPC_URL = os.getenv("RPC_URL")
 
 w3 = Web3(Web3.HTTPProvider(RPC_URL))
 
-with open('jsons/distributor_abi.json') as f:
+with open("jsons/distributor_abi.json") as f:
     distributor_abi = json.load(f)
 
-distributor = w3.eth.contract(address='0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae', abi=distributor_abi)
+distributor = w3.eth.contract(
+    address="0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae", abi=distributor_abi
+)
 CLAIMER = "0x3146e7bCeE81aE5a9BDcC8452ba7bBf9f8524205"
-TRUSTED_ADDRESS = os.getenv('TRUSTED_ADDRESS')
-TRUSTED_ADDRESS_KEY = os.getenv('TRUSTED_ADDRESS_KEY')
+TRUSTED_ADDRESS = os.getenv("TRUSTED_ADDRESS")
+TRUSTED_ADDRESS_KEY = os.getenv("TRUSTED_ADDRESS_KEY")
+
 
 async def send_txes():
     accounts_to_process = await get_accounts_without_operator()
@@ -25,26 +28,31 @@ async def send_txes():
 
     nonce = w3.eth.get_transaction_count(TRUSTED_ADDRESS)
     for i in range(0, len(accounts_to_process), 20):
-        batch = accounts_to_process[i:i+20]
+        batch = accounts_to_process[i : i + 20]
         batch_txes = []
         gas_price = w3.eth.gas_price
-        
+
         for account in batch:
-            tx = distributor.functions.toggleOperator(account, CLAIMER).build_transaction({
-                'from': TRUSTED_ADDRESS,
-                "maxFeePerGas": gas_price,
-                "nonce": nonce,
-                "chainId": 8453
-            })
-            nonce += 1
+            tx = distributor.functions.toggleOperator(
+                account, CLAIMER
+            ).build_transaction(
+                {
+                    "from": TRUSTED_ADDRESS,
+                    "maxFeePerGas": int(gas_price * 1.2),
+                    "maxPriorityFeePerGas": int(0.001 * 1e9),
+                    "nonce": nonce,
+                    "chainId": 8453,
+                }
+            )
             tx_signed = w3.eth.account.sign_transaction(tx, TRUSTED_ADDRESS)
             tx_hash = w3.eth.send_raw_transaction(tx_signed.rawTransaction)
+            nonce += 1
             print(tx_hash.hex())
             batch_txes.append(tx_hash.hex())
-        
+
         for tx_hash in batch_txes:
             w3.eth.wait_for_transaction_receipt(tx_hash)
-        
+
 
 if __name__ == "__main__":
     asyncio.run(send_txes())
